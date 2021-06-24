222570e54e96a9385c3b002863347e362115169c
Khadjia Okbi <54628700+khadijaokbi@users.noreply.github.com>
2021-06-24 10:11:12 +0200

    Add files via upload

---
+++ src/components/TodoListe.vue
@@ -0,0 +1,28 @@
+<template>
+<input type="text" size="50" v-model ="zielEingabe">
+<button @click="neuZiel">Neu</button>
+<h2>{{ msg }}</h2>
+<p v-for="ziel in zielListe" :key="ziel.key">
+<input type="radio" name="zielSelect" v-bind:value="ziel.id" v-model=zielSelect> 
+<button @click="deleteZiel">Delete</button>
+</p>
+</template>
+<script>
+export default {
+name: 'ToDo',
+props: { 
+    msg: String 
+    },
+data: function() {
+return { 
+    zielListe: [
+        {id:1, inhalte: 'CSS Animationen lernen'}
+    ]
+}
+},
+methods: {
+neuZiel() { },
+deleteZiel () { }
+}
+}
+</script>'},
