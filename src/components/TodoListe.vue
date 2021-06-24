bd06e36481e91479db3d6ade27ed0e586922590e
Khadjia Okbi <54628700+khadijaokbi@users.noreply.github.com>
2021-06-24 10:43:31 +0200

    Update TodoListe.vue

--- src/components/TodoListe.vue
+++ src/components/TodoListe.vue
@@ -4,8 +4,8 @@
 <h2>{{ msg }}</h2>
 <p v-for="ziel in zielListe" :key="ziel.key">
 <input type="radio" name="zielSelect" v-bind:value="ziel.id" v-model=zielSelect> 
-<button @click="deleteZiel">Delete</button>
 </p>
+<button @click="deleteZiel">Delete</button>
 </template>
 <script>
 export default {
@@ -16,13 +16,9 @@ props: {
 data: function() {
 return { 
     zielListe: [
-        {id:1, inhalte: 'CSS Animationen lernen'}
-    ]
+        {id:1, inhalt: 'CSS Animationen lernen'}
+        {id:2, inhalt: 'Vue lernen'}
+    ],
+    let neuId = this.zielListe[thiszielListe.length-1].id+1
 }
-},
-methods: {
-neuZiel() { },
-deleteZiel () { }
-}
-}
-</script>'},
+</script>
